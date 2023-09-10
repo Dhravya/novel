c3b1de7c9ad7c6b4ce5153ad1ff6b1da6d723a0b
Dhravya Shah <dhravyashah@gmail.com>
2023-09-10 04:00:56 +0000

    added word and char count

--- apps/web/ui/editor.tsx
+++ apps/web/ui/editor.tsx
@@ -6,18 +6,35 @@ import { Editor as NovelEditor } from "novel";
 export default function Editor() {
   const [saveStatus, setSaveStatus] = useState("Saved");
 
+  const [charCount, setCharCount] = useState(0);
+  const [wordCount, setWordCount] = useState(0);
+
   return (
     <div className="relative w-full max-w-screen-lg">
       <div className="absolute right-5 top-5 z-10 mb-5 rounded-lg bg-stone-100 px-2 py-1 text-sm text-stone-400">
-        {saveStatus}
+        {saveStatus} {charCount} - {wordCount}
       </div>
       <NovelEditor
-        onUpdate={() => {
+        onUpdate={test => {
+          console.log(test)
           setSaveStatus("Unsaved");
         }}
-        onDebouncedUpdate={() => {
+        onDebouncedUpdate={()=> {
           setSaveStatus("Saving...");
           // Simulate a delay in saving.
+
+          const content = localStorage.getItem("novel__content");
+        // its a json
+        const parsed = JSON.parse(content);
+          let totalCharCount = 0
+          let totalWordCount = 0
+          parsed.content.forEach(element => {
+            totalCharCount += element?.content?.[0]?.text?.length;
+            totalWordCount += element?.content?.[0]?.text?.split(" ")?.length;
+          });
+          setCharCount(totalCharCount)
+          setWordCount(totalWordCount)
+
           setTimeout(() => {
             setSaveStatus("Saved");
           }, 500);
